a72a92b69b50d68dfbcc9d96e6282606189bcdc5
kuldeep230 <ramdeep214@gmail.com>
2021-09-10 00:38:24 +0530

    Implemented hover effect on all the Icons

---
+++ CSS-Glassmorphism/hoverEffect-w-magicLine/js/main.js
@@ -0,0 +1,26 @@
+let marker = document.querySelector("#marker");
+let list = document.querySelectorAll("ul li");
+
+function moveIndicator(e) {
+  marker.style.left = e.offsetLeft + "px";
+  marker.style.width = e.offsetWidth + "px";
+}
+
+list.forEach((link) => {
+  link.addEventListener("mousemove", (e) => {
+    moveIndicator(e.target);
+  });
+});
+
+//add active class in hovered list item
+
+function activeLink() {
+  list.forEach((item) => {
+    item.classList.remove("active");
+    this.classList.add("active");
+  });
+}
+
+list.forEach((item) => {
+  item.addEventListener("mouseover", activeLink);
+});
